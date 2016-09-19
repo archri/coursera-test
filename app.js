6787fe6a36fe467041f54559a6e333d4054ac823
Hristo Hristov <archri@openmailbox.org>
2016-09-19 23:04:59 +0300

    lecture 13 part 3: simplifying the filter application

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 (function() {
   'use strict';
 
-  angular.module('NameCalculator', [])
-  .filter('cipher', cipherFilterFactory)
-  .controller('NameCalculatorController', function ($scope, cipherFilter) {
-    $scope.name = "";
-    $scope.totalValue = 0;
-    $scope.displayNumeric = function() {
-      var totalNameValue = calculateNumerticForString($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
+  angular.module('SomeApplication', [])
+  .filter('replaceSome', replaceSomeFilterFactory);
 
-    function calculateNumerticForString(string) {
-      var totalStringValue = 0;
-      for (var i = 0; i < string.length; i++) {
-        totalStringValue += string.charCodeAt(i);
-      }
-      var message
-          = "Total Numeric value of person`s name is: " + totalStringValue;
-      return cipherFilter(message);
+  function replaceSomeFilterFactory() {
+    return function (input) {
+      input = input || "";
+      return input.replace(/some/g, "another");
     };
-  });
-
-  function cipherFilterFactory() {
-    return function(message) {
-      return message.replace(/\s/g, "pi");
-    }
   }
 })();
